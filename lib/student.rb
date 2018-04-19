7948089dc3b0f9b5458964f7baea8da902d0636c
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-19 18:55:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    student = self.new(row[0], row[1])
-    student.id = row[2]
+    student = self.new(row[1], row[2])
+    student.id = row[0]
     return student
   end
 
